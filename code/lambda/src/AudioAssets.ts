0ffb44618fb16d24ac408cf7f624f81ecef22cdc
Miguel Ruiz <mig.ruiz@gmail.com>
2018-08-10 20:00:37 +0100

    los 4

--- code/lambda/src/AudioAssets.ts
+++ code/lambda/src/AudioAssets.ts
@@ -10,7 +10,7 @@ let en = {
             smallImageUrl: 'https://alexademo.ninja/skills/logo-108.png'
         }
     },
-    url: 'https://drive.google.com/uc?export=download&id=1kZsT3dqgK1-u9GKiBOeXg_9lkKM5jef0'
+    url: 'https://drive.google.com/uc?export=download&id=1xOJ-LlyVbpUY3p7-ddrSrXlIoSYrJe83'
 };
 
 
