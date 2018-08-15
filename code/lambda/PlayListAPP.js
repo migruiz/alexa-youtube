04b6bb9dd58ea86a6ff47a381ff0b82c9e0deea8
Miguel Ruiz <miguel@dpsystems.ie>
2018-08-15 12:20:09 +0100

    using ES6 Classes

--- code/lambda/PlayListAPP.js
+++ code/lambda/PlayListAPP.js
@@ -4,87 +4,94 @@ var dynamodb = new AWS.DynamoDB();
 var docClient = new AWS.DynamoDB.DocumentClient();
 var table = "Audio-Player-Multi-Stream";
 
-module.exports=function PlaylistAPP(){
-   
-    this.appId;
-    this.playlistId='';
-    this.playlist=[];
-    this.state;
-    this.initAppAsync=async function(playlistId){        
+
+function getPrevSong(playlist,songId){            
+    for (let index = 0; index < playlist.length; index++) {
+        const song = playlist[index];
+        if (song.id===songId){
+            var prevSongIndex=index-1;
+            if (prevSongIndex>=0){
+                return playlist[prevSongIndex];
+            }
+            else{
+                return playlist[0];
+            }
+        }            
+    }
+    return this.playlist[0];
+}
+
+function getNextSong(playlist,songId){
+    for (let index = 0; index < playlist.length; index++) {
+        const song = playlist[index];
+        if (song.id===songId){
+            var nextSongIndex=index+1;
+            if (nextSongIndex<playlist.length){
+                return playlist[nextSongIndex];
+            }
+            else{
+                return playlist[0];
+            }
+        }            
+    }
+    return playlist[0];
+}
+function getSongInfo(playlistId,song){        
+    return {
+        song:song,
+        token:JSON.stringify({appType:'PlayListAPP',playlistId:playlistId, songId:song.id})      
+    };
+}
+
+class PlaylistAPP {
+    constructor() {
+      this.appId=null;
+      this.playlistId=null;
+      this.playlist=null;
+      this.state=null;
+    }
+    async initAppAsync(playlistId){        
         this.playlistId=playlistId;
         this.appId='APPS/PlaylistAPP/'+playlistId;
         this.playlist=await getyoutubePlaylistAsync(playlistId);
         this.state=await getAppState(playlistId);
-    }    
-    getSongInfo=function(playlistId,song){
-        return {
-            song:song,
-            token:JSON.stringify({appType:'PlayListAPP',playlistId:playlistId, songId:song.id})      
-        };
     }
 
-    this.getLastPlayedSongInfo=function(){
+    getLastPlayedSongInfo(){
         if (this.state){
             for (let index = 0; index < this.playlist.length; index++) {
                 const song = this.playlist[index];
                 if (song.id===this.state.currentPlayingSongId){
-                    return getSongInfo(this.playlistId,song);
+                    return getSongInfo(playlistId,song);
                 }
             }
             var firstSong=this.playlist[0];
-            return getSongInfo(this.playlistId,firstSong)
+            return getSongInfo(playlistId,firstSong)
         }
         else{
             var firstSong=this.playlist[0];
-            return getSongInfo(this.playlistId,firstSong)
+            return getSongInfo(playlistId,firstSong)
         }
     }
-    this.getPrevSongInfo=function(){
+    getPrevSongInfo(){
         var prevSong=getPrevSong(this.playlist,this.state.currentPlayingSongId);
-        return getSongInfo(this.playlistId,prevSong);
-        function getPrevSong(playlist,songId){
-            for (let index = 0; index < playlist.length; index++) {
-                const song = playlist[index];
-                if (song.id===songId){
-                    var prevSongIndex=index-1;
-                    if (prevSongIndex>=0){
-                        return playlist[prevSongIndex];
-                    }
-                    else{
-                        return playlist[0];
-                    }
-                }            
-            }
-            return this.playlist[0];
-        }
+        return getSongInfo(playlistId,prevSong);
+        
     }
-    this.getNextSongInfo=function(){
+
+    getNextSongInfo(){
         var nextSong=getNextSong(this.playlist,this.state.currentPlayingSongId);
-        return getSongInfo(this.playlistId,nextSong);
-        function getNextSong(playlist,songId){
-            for (let index = 0; index < playlist.length; index++) {
-                const song = playlist[index];
-                if (song.id===songId){
-                    var nextSongIndex=index+1;
-                    if (nextSongIndex<playlist.length){
-                        return playlist[nextSongIndex];
-                    }
-                    else{
-                        return playlist[0];
-                    }
-                }            
-            }
-            return playlist[0];
-        }
+        return getSongInfo(playlistId,nextSong);
+
     }
 
 
-    this.setCurrentPlayingSong=function(songId,offset){
+    setCurrentPlayingSong(songId,offset){
         this.state={};
         this.state.currentPlayingSongId=songId;
         this.state.offset=offset;
     }
-    this.saveStateAsync=async function(){
+    async saveStateAsync(){
         var dynamoParams = {
             TableName:table,
             Item:{
@@ -94,49 +101,12 @@ module.exports=function PlaylistAPP(){
         };
         await putAsync(dynamoParams);
     }
-}
-
-async function getAppState(playlistId){
-    var id='APPS/PlaylistAPP/'+playlistId;
-    var params = {
-        TableName: table,
-        Key:{
-            "id":id
-        }
-    };
-    var data= await getAsync(params);
-    return data?data.Item.appState:null;
-}
-async function getyoutubePlaylistAsync(playlistId){
-    var id='youtubeplaylists/'+playlistId;
-    var params = {
-        TableName: table,
-        Key:{
-            "id":id
-        }
-    };
-    var data= await getAsync(params);
-    return data?data.Item.items:[];
-}
 
 
-async function putAsync(params){
-    return new Promise(function (resolve, reject) {
+  }
 
-        docClient.put(params, function(err, data) {
-            if (err !== null) return reject(err);
-            resolve(data);
-        });
-    });
 
-}
-async function getAsync(params){
-    return new Promise(function (resolve, reject) {
 
-        docClient.get(params, function(err, data) {
-            if (err !== null) return reject(err);
-            resolve(data);
-        });
-    });
 
-}
+
+module.exports=PlaylistAPP;
